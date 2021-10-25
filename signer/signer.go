97be524052c9140795f7ffba92a8289fb54a5489
Ivan Cheremisin <fess932@gmail.com>
2021-10-25 17:04:19 +0300

    signer

--- signer/signer.go
+++ signer/signer.go
@@ -1,3 +1,31 @@
 package main
 
+import "log"
+
 // сюда писать код
+func ExecutePipeline(jobs ...job) {
+
+	in := make(chan interface{})
+	out := make(chan interface{})
+
+	for _, j := range jobs {
+		j(in, out)
+	}
+
+}
+
+func SingleHash(in, out chan interface{}) {
+	for v := range in {
+		hash := DataSignerCrc32(v.(string))
+		log.Println("kek", hash)
+		out <- hash
+	}
+}
+
+func MultiHash(in, out chan interface{}) {
+
+}
+
+func CombineResults(in, out chan interface{}) {
+
+}
