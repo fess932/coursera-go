d0cd649636883d440cdf137359b9a13de68e7cd3
Ivan Cheremisin <fess932@gmail.com>
2021-10-27 10:18:56 +0300

    signer

--- signer/signer.go
+++ signer/signer.go
@@ -8,46 +8,67 @@ import (
 	"time"
 )
 
-//func main() {
-//	inputData := []int{1, 2}
-//
-//	hashSignJobs := []job{
-//		job(func(in, out chan interface{}) {
-//			for _, fibNum := range inputData {
-//				out <- fibNum
-//			}
-//		}),
-//		job(SingleHash),
-//		job(MultiHash),
-//		job(CombineResults),
-//		job(func(in, out chan interface{}) {
-//			dataRaw := <-in
-//			log.Println(dataRaw)
-//		}),
-//	}
-//
-//	start := time.Now()
-//
-//	ExecutePipeline(hashSignJobs...)
-//
-//	end := time.Since(start)
-//
-//	log.Println("time:", end)
-//}
+func main() {
+	inputData := []int{0, 1, 1, 2, 3, 5, 8}
+
+	hashSignJobs := []job{
+		job(func(in, out chan interface{}) {
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
+		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
+		job(func(in, out chan interface{}) {
+			<-in
+		}),
+	}
+
+	start := time.Now()
+
+	ExecutePipeline(hashSignJobs...)
+
+	end := time.Since(start)
+
+	log.Println("time:", end)
+}
 
 func ExecutePipeline(jobs ...job) {
-	var wg sync.WaitGroup
 
 	in := make(chan interface{})
+	out := make(chan interface{})
 
-	for _, j := range jobs {
-		in = run(in, j, &wg)
-	}
+	go jobs[0](make(chan interface{}), in)
+
+	var wg sync.WaitGroup
+	go runProcessJobs(&wg, out, jobs[1:]...)
+
+	func() {
+	loop:
+		for {
+			select {
+			case v := <-in:
+				print("ok")
+				out <- v
+			case <-time.After(time.Millisecond * 10):
+				fmt.Println("No input, close pipeline")
+				close(in)
+				break loop
+			}
+		}
+	}()
 
 	wg.Wait()
 }
 
-func run(in chan interface{}, j job, wg *sync.WaitGroup) (out chan interface{}) {
+func runProcessJobs(wg *sync.WaitGroup, in chan interface{}, jobs ...job) {
+	for _, j := range jobs {
+		in = run(wg, in, j)
+	}
+}
+
+func run(wg *sync.WaitGroup, in chan interface{}, j job) (out chan interface{}) {
 	out = make(chan interface{})
 
 	wg.Add(1)
@@ -60,62 +81,67 @@ func run(in chan interface{}, j job, wg *sync.WaitGroup) (out chan interface{}) 
 }
 
 func SingleHash(in, out chan interface{}) {
-	time.Sleep(time.Millisecond)
-
-loop:
 	for {
-		select {
-		case i := <-in:
-			fmt.Printf("Value %d was read.\n", i)
-
-			var data string
-
-			switch d := i.(type) {
-			case string:
-				data = d
-			case int:
-				data = strconv.Itoa(d)
-			default:
-				log.Println("wrong type assertion single hash:", i)
-			}
-
-			log.Println("data:", data)
-
-			var wg sync.WaitGroup
-
-			var p1, p2 string
-
-			wg.Add(1)
-			go func() {
-				p1 = DataSignerCrc32(data) // gorutine
-				wg.Done()
-			}()
-
-			wg.Add(1)
-			go func() {
-				time.Sleep(time.Millisecond * 10)
-				h2 := DataSignerMd5(data) // gorutine
-				p2 = DataSignerCrc32(h2)  // gorutine
-				wg.Done()
-			}()
-
-			wg.Wait()
-
-			hash := p1 + "~" + p2
-			out <- hash
-			log.Println("single", hash)
-		default:
-			fmt.Println("No value ready, moving on.")
-			break loop
+		i, ok := <-in
+		if !ok {
+			log.Println("closed channel")
+			break
 		}
+
+		fmt.Printf("Value %d was read.\n", i)
+
+		var data string
+
+		switch d := i.(type) {
+		case string:
+			data = d
+		case int:
+			data = strconv.Itoa(d)
+		default:
+			log.Println("wrong type assertion single hash:", i)
+		}
+
+		log.Println("data:", data)
+
+		var wg sync.WaitGroup
+
+		var p1, p2 string
+
+		wg.Add(1)
+		go func() {
+			p1 = DataSignerCrc32(data) // gorutine
+			wg.Done()
+		}()
+
+		wg.Add(1)
+
+		time.Sleep(time.Millisecond * 10)
+		h2 := DataSignerMd5(data) // gorutine
+
+		go func() {
+			p2 = DataSignerCrc32(h2) // gorutine
+			wg.Done()
+		}()
+
+		wg.Wait()
+
+		hash := p1 + "~" + p2
+		out <- hash
+		log.Println("single", hash)
 	}
 
 	close(out)
 }
 
 func MultiHash(in, out chan interface{}) {
-	for v := range in {
-		data, ok := v.(string)
+	for {
+		i, ok := <-in
+		if !ok {
+			log.Println("closed channel")
+			break
+		}
+
+		data, ok := i.(string)
 		if !ok {
 			log.Println("wrong type assertion")
 		}
@@ -162,8 +188,7 @@ func CombineResults(in, out chan interface{}) {
 			total += "_" + data
 		}
 
-		log.Println("combine", total)
 	}
-	log.Println("end")
+	log.Println("combine", total)
 	out <- total
 }
